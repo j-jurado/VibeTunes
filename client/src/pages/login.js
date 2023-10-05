921799831269bad025b856a7d9ac80167f82d88a
j-jurado <73544267+j-jurado@users.noreply.github.com>
2023-10-05 12:18:13 -0400

    Boilerplate Code

---
+++ client/src/pages/login.js
@@ -0,0 +1,12 @@
+function Login() {
+    return (
+      <div className="login">
+        <h1>VibeTunes</h1>
+        <p>Login Page</p>
+        <button>Login With Spotify</button>
+      </div>
+    );
+  }
+  
+  export default Login;
+  
--- server/index.js
+++ server/index.js
@@ -5,3 +5,16 @@ const port = 3001;
 app.listen(port, () => {
     console.log("SERVER INITIATED SUCCESS FULLY ON PORT: " + port);
 });
+
+app.get("/", (req, res) => {
+    res.send(
+      "<a href='https://accounts.spotify.com/authorize?client_id=" +
+        process.env.CLIENT_ID +
+        "&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A8080%2Faccount&scope=user-top-read'>Sign in</a>"
+    );
+  });
+  
+app.get("/account", async (req, res) => {
+console.log("spotify response code is " + req.query.code);
+res.send("account page");
+});
